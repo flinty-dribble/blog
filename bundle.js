5741528e9841ce5a048636b95ad342b1f6a29036
flinty-dribble <flinty-dribble@users.noreply.github.com>
2022-04-10 07:32:37 +0000

    Deploying to gh-pages from  @ 275e259ef781101ce1c8e2df3c6a8e90f81667e9 🚀

---
+++ bundle.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.querySelector("h1").innerHTML="Всем привет! Это мой блог."}();
+//# sourceMappingURL=bundle.js.map
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./style/style.css";
-
-const h1 = document.querySelector("h1");
-h1.innerHTML = "Всем привет! Это мой блог.";
